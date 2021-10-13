0de73b6966e8a2ef4c2f001f92691ece2211f170
Talal Siddiqui <35397160+talalsidd@users.noreply.github.com>
2021-10-13 16:55:16 +0500

    Add files via upload
    List page IDs extraction from zillow.com

---
+++ listpagenew_updated.js
@@ -0,0 +1,91 @@
+window.mainurls = ["https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A2%7D%2C%22usersSearchTerm%22%3A%22PA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-81.922324453125%2C%22east%22%3A-73.287070546875%2C%22south%22%3A40.02728171352374%2C%22north%22%3A42.220059996038025%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A47%2C%22regionType%22%3A2%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22doz%22%3A%7B%22value%22%3A%221%22%7D%2C%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%2C%22isCondo%22%3A%7B%22value%22%3Afalse%7D%2C%22isMultiFamily%22%3A%7B%22value%22%3Afalse%7D%2C%22isManufactured%22%3A%7B%22value%22%3Afalse%7D%2C%22isLotLand%22%3A%7B%22value%22%3Afalse%7D%2C%22isTownhouse%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartment%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartmentOrCondo%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A7%7D&wants={%22cat1%22:[%22listResults%22,%22mapResults%22],%22cat2%22:[%22total%22]}&requestId=6","https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A2%7D%2C%22usersSearchTerm%22%3A%22CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-153.847663625%2C%22east%22%3A-84.765632375%2C%22south%22%3A27.621171317999984%2C%22north%22%3A46.085364881491216%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A9%2C%22regionType%22%3A2%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22doz%22%3A%7B%22value%22%3A%221%22%7D%2C%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%2C%22isCondo%22%3A%7B%22value%22%3Afalse%7D%2C%22isMultiFamily%22%3A%7B%22value%22%3Afalse%7D%2C%22isManufactured%22%3A%7B%22value%22%3Afalse%7D%2C%22isLotLand%22%3A%7B%22value%22%3Afalse%7D%2C%22isTownhouse%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartment%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartmentOrCondo%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A4%7D&wants={%22cat1%22:[%22listResults%22,%22mapResults%22],%22cat2%22:[%22total%22]}&requestId=4","https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A2%7D%2C%22usersSearchTerm%22%3A%22FL%22%2C%22mapBounds%22%3A%7B%22west%22%3A-101.0751078125%2C%22east%22%3A-66.5340921875%2C%22south%22%3A22.479049303094325%2C%22north%22%3A32.77525637831931%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A14%2C%22regionType%22%3A2%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22doz%22%3A%7B%22value%22%3A%221%22%7D%2C%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%2C%22isCondo%22%3A%7B%22value%22%3Afalse%7D%2C%22isMultiFamily%22%3A%7B%22value%22%3Afalse%7D%2C%22isManufactured%22%3A%7B%22value%22%3Afalse%7D%2C%22isLotLand%22%3A%7B%22value%22%3Afalse%7D%2C%22isTownhouse%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartment%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartmentOrCondo%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A5%7D&wants={%22cat1%22:[%22listResults%22,%22mapResults%22],%22cat2%22:[%22total%22]}&requestId=4","https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A2%7D%2C%22usersSearchTerm%22%3A%22NV%22%2C%22mapBounds%22%3A%7B%22west%22%3A-134.2935673125%2C%22east%22%3A-99.7525516875%2C%22south%22%3A33.89368753441963%2C%22north%22%3A42.99300880690719%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A42%2C%22regionType%22%3A2%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22doz%22%3A%7B%22value%22%3A%221%22%7D%2C%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%2C%22isCondo%22%3A%7B%22value%22%3Afalse%7D%2C%22isMultiFamily%22%3A%7B%22value%22%3Afalse%7D%2C%22isManufactured%22%3A%7B%22value%22%3Afalse%7D%2C%22isLotLand%22%3A%7B%22value%22%3Afalse%7D%2C%22isTownhouse%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartment%22%3A%7B%22value%22%3Afalse%7D%2C%22isApartmentOrCondo%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A5%7D&wants={%22cat1%22:[%22listResults%22,%22mapResults%22],%22cat2%22:[%22total%22]}&requestId=4"]
+window.totalpages = 25;
+window._jsonData = null;
+//window._jsonData = {};
+var FailedFetchRequests = [];
+window.jsondata = '';
+window.jsondata2 = '';
+window.purls = [];
+window.alldata = ['Zillow ID\tURL\tPRICE'];
+//window.data2=['ZILLOW ID\tCONTACT\tPOSTING DATE\tAGENT\tALL FACTS\tBASEMENT\tCOOLING\tHEATING\tLAUNDARY\tFIRE PLACES\tPARKING\tGARAGE\tROOF TYPE\tYEAR BUILT\tZESTIMATE\tCITY\tZIP CODE\tSTATE\tADDRESS\tVIEWS COUNT\tSAVES COUNT\tTIME ON ZILLOW']
+window.maindata = []
+
+
+window.allurls = [];
+
+
+for (var urlloop = 0; urlloop < window.mainurls.length  ; urlloop++){
+		var caturl1 = window.mainurls[urlloop].replace(/.*currentPage%22%3A\d+/,'')
+		var caturl2 = window.mainurls[urlloop].replace(/\d+%7D%2C%22usersSearchTerm.*/,'')
+		window.Refferer = window.location.href
+
+		var linkslooper = 0;
+		for (linkslooper = 0; linkslooper < window.totalpages; linkslooper++){
+			window.allurls.push(caturl2 + linkslooper + caturl1);
+		}
+}
+
+
+for (var linkslooper = 0; linkslooper < window.allurls.length; linkslooper++){
+	doSetTimeout(linkslooper,window.allurls[linkslooper]);
+}
+
+
+function doSetTimeout(linkslooper,currenturl) {
+			setTimeout(function() {
+				fetchRequestfunction(linkslooper,currenturl);
+			}, linkslooper*1500);
+		}
+
+
+fetchRequestfunction = function(linkslooper,currenturl) {
+			fetch(currenturl, {
+			"headers": {
+			"accept": "*/*",
+			"accept-language": "en-US,en;q=0.9",
+			"sec-ch-ua": "\"Google Chrome\";v=\"93\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"93\"",
+			"sec-ch-ua-mobile": "?0",
+			"sec-ch-ua-platform": "\"Windows\"",
+			"sec-fetch-dest": "empty",
+			"sec-fetch-mode": "cors",
+			"sec-fetch-site": "same-origin"
+		  },
+		  "referrer": Refferer,
+		  "referrerPolicy": "unsafe-url",
+		  "body": null,
+		  "method": "GET",
+		  "mode": "cors",
+		  "credentials": "include"
+		})
+			.then(response => response.json())
+			.then(JSONObject => {
+				window.jsondata = JSONObject;
+			})
+				.then(function() {
+                    try{
+                        for (var i = 0 ; i < window.jsondata.cat1.searchResults.listResults.length ; i++){
+                            var ZPID = window.jsondata.cat1.searchResults.listResults[i].zpid;
+                            var PRICE = window.jsondata.cat1.searchResults.listResults[i].price;
+                            var ZILLOW_URL = window.jsondata.cat1.searchResults.listResults[i].detailUrl;               
+                            window.alldata.push(ZPID + '\t' +ZILLOW_URL +'\t'+ PRICE);
+                            window.purls.push(ZPID)
+                            
+                        }
+
+                    }
+                    catch(err){
+                        for (var i = 0 ; i < window.jsondata.cat1.searchResults.mapResults.length ; i++){
+                            var ZPID = window.jsondata.cat1.searchResults.mapResults[i].zpid;
+                            var PRICE = window.jsondata.cat1.searchResults.mapResults[i].price;
+                            var ZILLOW_URL = 'https://www.zillow.com/'+window.jsondata.cat1.searchResults.mapResults[i].detailUrl;               
+                            window.alldata.push(ZPID + '\t' +ZILLOW_URL +'\t'+ PRICE);
+                            window.purls.push(ZPID)
+                            
+                        }
+
+                    }
+					
+                    //console.log(alldata);
+					console.log(linkslooper);
+				});
+}
